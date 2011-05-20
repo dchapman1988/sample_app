a1b69eb008f8e389122e0ddfc1f6b36a69d6bf3d
David Chapman <david@isotope11.com>
2011-05-20 08:01:52 -0500

    Added tests to make sure links go where they are suppose to.

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
 
+  # Tests to make sure paths have the right routes
   it "should have a Home page at '/'" do
     get '/'
     response.should have_selector('title', :content => "Home")
@@ -24,6 +25,21 @@ describe "LayoutLinks" do
 
   it "should have a signup page at '/signup'" do
     get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
+
+  # Test for making sure links go where they're suppose to.
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
+    click_link "Sign up now!"
     response.should have_selector('title', :content => "Sign up")
   end
 end
